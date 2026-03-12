616bbf1e087e346ba2f0b8621d9da6c2036d2e09
summerradkiewicz <summerradkiewicz@u.boisestate.edu>
2026-03-12 14:41:48 +0000

    finished lab

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,54 @@
-function domLoaded() {
+let textArea;
+let ol;
+
+window.addEventListener('DOMContentLoaded', function domLoaded() {
     // register addBtnClick() as Add button's click event handler
-}
+    const addBtn = document.getElementById('add-btn');
+    addBtn.addEventListener('click', addBtnClick);
+
+    // let the enter key also trigger addBtnClick()
+    textArea = document.getElementById('text-area');
+    textArea.addEventListener('keyup', (event) => {
+        if (event.key == 'Enter') {
+            console.log("enter");
+            addBtnClick();
+        }
+    });
+});
 
 function addBtnClick() {
     // extract the text entered in the textbox, then call addTask() with the new task
+    textArea = document.getElementById('text-area');
+    const newTask = textArea.value;
+
+    // if text was entered, add it as a task and clear the textbox
+    if (textArea.value != "") {
+        textArea.value = "";
+        addTask(newTask);
+    }
+
+    textArea.focus(); // put focus back on the box
 }
 
-function addTask() {
+function addTask(task) {
     // Call document.createElement() to create a new <li> element
-    // Set the new <li> element's innerHTML to the following string where NEW TASK is the addTask() parameter:
-    //         <span class="task-text">NEW TASK</span><button class="done-btn">&#10006;</button>
+    const listItem = document.createElement('li');
+    const taskHTML = '<span class="task-text">' + task + '</span><button class="done-btn">&#10006;</button>';
+    // Set the new <li> element's innerHTML
+    listItem.innerHTML = taskHTML;
+
     // Call document.querySelector() to find the <ol> DOM node
+    ol = document.querySelector('ol');
     // Call appendChild() on the <ol> DOM node with the new <li> element to append the <li> to the ordered list
+    ol.appendChild(listItem);
+
+    // add removeTask() to any new task
+    const doneBtnList = document.querySelectorAll('.done-btn');
+    const lastBtn = doneBtnList[doneBtnList.length - 1];
+    lastBtn.addEventListener('click', removeTask);
+}
+
+function removeTask(event) {
+    const li = event.target.parentNode;
+    ol.removeChild(li);
 }
